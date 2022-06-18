8898b02306e7c6b0df728f8002f39a58d8fe2208
f1lrotto <filip.hupka@gmail.com>
2022-06-18 05:49:44 +0200

    added int type for port

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def request():
 if __name__ == "__main__":
     from waitress import serve
     print(f'Server is running on port {PORT}')
-    serve(app, host="0.0.0.0", port=os.environ.get("PORT"))
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT")))
